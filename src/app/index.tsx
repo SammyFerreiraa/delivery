cd0b6c57fb8054301de7379f710d25122cbbb46c
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-06 12:36:41 -0300

    :sparkles: feat: loading component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import { View, Text } from 'react-native'
 
 export default function App() {
   return (
     <View className="flex-1 pt-8">
-      <Text className="text-zinc-50 text-2xl">Hello React Native</Text>
+      <Text className="text-zinc-50 text-2xl font-bold">Hello React Native</Text>
     </View>
   )
 }
