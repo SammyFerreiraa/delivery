074dc83eaa616e084c08395440931cb43010e15e
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-06 20:04:35 -0300

    :sparkles: feat: button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,42 @@
+import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
+
+type ButtonTextProps = {
+  children: React.ReactNode
+}
+
+type ButtonIconProps = {
+  children: React.ReactNode
+}
+
+type ButtonProps = TouchableOpacityProps & {
+  children: React.ReactNode
+}
+
+function Button({ children, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity
+      {...rest}
+      className="h-12 bg-lime-400 rounded-md items-center justify-center flex-row"
+      activeOpacity={0.7}
+    >
+      {children}
+    </TouchableOpacity>
+  )
+}
+
+function ButtonText({ children }: ButtonTextProps) {
+  return (
+    <Text className="text-base font-heading text-black mx-2">
+      {children}
+    </Text>
+  )
+}
+
+function ButtonIcon({ children }: ButtonIconProps) {
+  return children
+}
+
+Button.Text = ButtonText
+Button.Icon = ButtonIcon
+
+export { Button }
