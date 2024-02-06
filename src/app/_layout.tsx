dd5923c86bae5f770670f335dbcff1db9b16506a
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-06 12:36:12 -0300

    :lipstick: feat: fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,25 @@
 import { Slot } from "expo-router";
 import { SafeAreaView, SafeAreaViewBase } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter'
+import { Loading } from "@/components/loading";
 
 export default function Layout() {
+  const [fontsLoader] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold
+  })
+
+  if (!fontsLoader) {
+    return <Loading />
+  }
   return (
     <SafeAreaView className="flex-1 bg-zinc-900">
       <Slot />
