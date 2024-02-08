3e4ff8d1241de737c173da2409f4f07449044b19
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-08 10:14:37 -0300

    :recycle|: refactor: title and find

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -5,6 +5,7 @@ import { formatCurrency } from "@/utils/functions/formatCurrency";
 import { Button } from "@/components/Button";
 import { Feather } from '@expo/vector-icons'
 import { LinkButton } from "@/components/LinkButton";
+import { Redirect } from 'expo-router'
 
 import { useCartStore } from "@/stores/CardStore";
 
@@ -13,7 +14,11 @@ export default function Product() {
   const navigation = useNavigation()
   const useStore = useCartStore()
 
-  const product = PRODUCTS.filter((product) => product.id === id)[0]
+  const product = PRODUCTS.find((product) => product.id === id)
+
+  if (!product) {
+    return <Redirect href="/" />
+  }
 
   const handleAddToCart = () => {
     useStore.add(product)
@@ -24,7 +29,9 @@ export default function Product() {
     <View className="flex-1">
       <Image source={product.cover} className="w-full h-52" resizeMode="cover" />
 
+
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading">{product.title}</Text>
         <Text className="text-lime-400 text-xl font-heading my-2">{formatCurrency(product.price)}</Text>
         <Text className="text-slate-400 font-body text-base leading-6 mb-6">{product.description}</Text>
 
