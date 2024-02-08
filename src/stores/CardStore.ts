bbdc33c2a2dc41da80cd4266eeab7afb9f1cde1b
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-08 12:03:51 -0300

    :sparkles: feat: asyncStorage in the smartphone

--- src/stores/CardStore.ts
+++ src/stores/CardStore.ts
@@ -1,6 +1,8 @@
 import { ProductProps } from '@/utils/data/products'
 import { create } from 'zustand'
 import * as CartInMemory from './helpers/CartInMemory'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
 export type ProductCartProps = ProductProps & {
   quantity: number
@@ -12,7 +14,8 @@ type StateProps = {
   remove: (productId: string) => void
 }
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(
+persist<StateProps>((set) => ({
   products: [],
   add: (product: ProductProps) => set((state) => ({
     products: CartInMemory.add(state.products, product)
@@ -20,4 +23,7 @@ export const useCartStore = create<StateProps>((set) => ({
   remove: (productId: string) => set((state) => ({
     products: CartInMemory.remove(state.products, productId)
   }))
+}), {
+  name: 'cart-storage',
+  storage: createJSONStorage(() => AsyncStorage)
 }))
